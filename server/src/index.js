c8f5ba554dff948e86efd0284067c39e3c56bbfe
D-Mastrocola <mastrodomcola@gmail.com>
2024-08-07 14:35:38 -0400

    installed express and seperated client and server

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const mysql = require('mysql');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Running on Port: ${PORT}`);
+});
