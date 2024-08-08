9f85645467a9631a7dc1eac576931d8fa921bd58
D-Mastrocola <mastrodomcola@gmail.com>
2024-08-08 15:17:46 -0400

    feature/user-database

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Kanit", "Noto Sans", "sans-serif"].join(","),
+  },
+});
 
 function App() {
   return (
-    <>
-      <Header/>
-    </>
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Profile />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
   );
 }
 
