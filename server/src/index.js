aa637ad74b95d0993c7009b32ff99312678598bf
D-Mastrocola <mastrodomcola@gmail.com>
2024-08-08 12:18:09 -0400

    user models and get and post routes work

--- server/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const mysql = require('mysql');
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Running on Port: ${PORT}`);
-});
